ec9cc30541e826b232fe86206b1ec868884a0a75
Ladislav Sokol <sokollac10@gmail.com>
2022-03-15 14:30:03 +0100

    login and register is done(maybe)

--- src/main/java/com/example/swapper/HelloApplication.java
+++ src/main/java/com/example/swapper/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/register.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 576);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
