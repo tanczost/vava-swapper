6adb14dd69a5f54a9e8e7abd8ed11d1f1611613b
LukasRucek <79168684+LukasRucek@users.noreply.github.com>
2022-03-23 11:35:57 +0100

    Adding Sigleton

--- src/main/java/com/example/swapper/LandingPageController.java
+++ src/main/java/com/example/swapper/LandingPageController.java
@@ -1,4 +1,41 @@
 package com.example.swapper;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import models.Singleton;
+
+import java.io.IOException;
+
+
+
 public class LandingPageController {
+    @FXML
+    private Button login;
+    @FXML
+    private Button addproduct;
+
+
+    @FXML
+    public void changeScenario(MouseEvent event) throws IOException {
+        ImageView Item = (ImageView) event.getSource();
+        Singleton.getInstance().setNameOfProduct(Item.getId());
+        HelloApplication main = new HelloApplication();
+        main.changeScene("views/categoryPage.fxml");
+
+    }
+
+
+    @FXML
+    public void initialize(){
+        boolean status = true;
+        if(status == true){
+            login.setVisible(true);
+        }
+        else {
+            addproduct.setVisible(true);
+        }
+    }
 }
