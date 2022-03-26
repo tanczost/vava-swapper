f2df2e3c9760e4df0cdae917ca12035389459781
tanczost <tanczost23@gmail.com>
2022-03-26 10:47:44 +0100

    update schema for db

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,16 @@
-CREATE TABLE friends (
-     id SERIAL PRIMARY KEY,
-     name VARCHAR(200) NOT NULL,
-     phone_number INTEGER NOT NULL
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+                       id serial PRIMARY KEY ,
+                       nick varchar(255) UNIQUE NOT NULL,
+                       first_name varchar(255) NOT NULL,
+                       last_name varchar(255) NOT NULL,
+                       email varchar(255) UNIQUE NOT NULL,
+                       password varchar(255) NOT NULL,
+                       town varchar(255) NOT NULL ,
+                       street varchar(255) NOT NULL ,
+                       school varchar(255) NOT NULL ,
+                       created_at TIMESTAMP NOT NULL
 );
 
-INSERT INTO friends  (name, phone_number) VALUES ('Tomi', 12321);
+ALTER TABLE users ALTER COLUMN created_at SET DEFAULT now();
