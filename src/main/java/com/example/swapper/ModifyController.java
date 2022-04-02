36345163302564b65094479863307f8c8aa39148
tanczost <tanczost23@gmail.com>
2022-04-02 14:57:54 +0200

    load img when user update

--- src/main/java/com/example/swapper/ModifyController.java
+++ src/main/java/com/example/swapper/ModifyController.java
@@ -6,10 +6,13 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import models.Account;
 import models.Product;
+import service.FileHandler;
 import service.db.ProductDbServices;
 import service.navigation.SwitchScreen;
-
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
@@ -25,15 +28,22 @@ public class ModifyController {
     @FXML
     private RadioButton rbTop;
 
+    @FXML
+    private ImageView imgView;
+
 
     @FXML
-    public void initialize() {
+    public void initialize() throws Exception {
         Product currentProduct = Account.getCurrentProduct();
         tfProductName.setText(currentProduct.getName());
         tfDescription.setText(currentProduct.getDescription());
         ResourceBundle resourceBundle = ResourceBundle.getBundle("resource_bundle");
         btnProposalPage.setText(resourceBundle.getString("myProposals"));
         btnUpdate.setText(resourceBundle.getString("update"));
+
+        InputStream is = FileHandler.getFile(currentProduct.getImgId());
+        Image img = new Image(is);
+        imgView.setImage(img);
     }
 
     @FXML
@@ -46,6 +56,8 @@ public class ModifyController {
         } else {
             System.out.println("Not  success do something");
         }
+
+        Account.loadProducts();
     }
 
     @FXML
--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -57,7 +57,7 @@ public class Account {
                     products.getString(2),
                     products.getString(3),
                     products.getBoolean(4),
-                    0
+                    products.getInt(5)
             ));
         }
         System.out.println("Products are successfully loaded into account");
