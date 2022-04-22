5ce4e0eeb21df7870a2b53a29e02ea3f6ce80708
LukasRucek <79168684+LukasRucek@users.noreply.github.com>
2022-04-22 20:58:37 +0200

    Updating OfferDetailPage

--- src/main/java/controllers/OfferDetailController.java
+++ src/main/java/controllers/OfferDetailController.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import models.Account;
 import observer.Observer;
 import observer.Subject;
@@ -20,6 +22,8 @@ import java.util.ResourceBundle;
 public class OfferDetailController extends Subject {
     @FXML
     public ImageView ivProduct;
+    @FXML
+    public ImageView tradeIcon;
     @FXML
     public Label lbProduct;
     @FXML
@@ -52,6 +56,7 @@ public class OfferDetailController extends Subject {
 
         InputStream is = FileHandler.getFile(account.getCurrentOffer().getImgId());
         ivProduct.setImage(new Image(is));
+        centerImage();
 
     }
 
@@ -63,5 +68,33 @@ public class OfferDetailController extends Subject {
 
     public void redirectBack() throws IOException {
         SwitchScreen.changeScreen("views/OfferPage.fxml");
+    }
+
+    @FXML
+    public void changeTrade(MouseEvent event){
+        Button Item = (Button) event.getSource();
+        Image newImage = new Image(getClass().getResourceAsStream("views/images/"+Item.getId()+"_success.png"));
+        Image oldImage = new Image(getClass().getResourceAsStream("views/images/"+Item.getId()+".png"));
+        tradeIcon.setImage(newImage);
+        EventHandler<MouseEvent> highlight = e -> tradeIcon.setImage(oldImage);
+        Item.setOnMouseExited(highlight);
+    }
+
+    public void centerImage() {
+        Image img = ivProduct.getImage();
+        if (img != null) {
+            double w,h, reduce;
+            double ratioX = ivProduct.getFitWidth() / img.getWidth();
+            double ratioY = ivProduct.getFitHeight() / img.getHeight();
+            if(ratioX >= ratioY) {
+                reduce = ratioY;
+            } else {
+                reduce = ratioX;
+            }
+            w = img.getWidth() * reduce;
+            h = img.getHeight() * reduce;
+            ivProduct.setX((ivProduct.getFitWidth() - w) / 2);
+            ivProduct.setY((ivProduct.getFitHeight() - h) / 2);
+        }
     }
 }
